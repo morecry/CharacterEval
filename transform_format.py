1be1239818a85af9adaa83cb85a76a4b1f4643bc
Quan Tu <45983142+morecry@users.noreply.github.com>
2024-01-10 13:53:30 +0800

    Update transform_format.py

--- transform_format.py
+++ transform_format.py
@@ -5,7 +5,7 @@ import copy
 with open('data/id2metric.jsonl','r') as f:
     id_metric = json.load(f)
 
-with open(f'results/generation_trans_baichuan_7b.jsonl','r') as f:
+with open(f'results/generation.jsonl','r') as f:
     datas = json.load(f)
 
 results = []

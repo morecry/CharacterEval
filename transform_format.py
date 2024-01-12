9f1a6ae0a4fcd160410c936659acaf50e8293536
Quan Tu <45983142+morecry@users.noreply.github.com>
2024-01-12 19:49:30 +0800

    Update transform_format.py

--- transform_format.py
+++ transform_format.py
@@ -11,14 +11,15 @@ with open(f'results/generation.jsonl','r') as f:
 results = []
 
 for data in datas:
-    if data['model_output'] != "ERROR":
+    if data['model_output'] is not None and data['model_output'] != "ERROR":
         model_output = data['model_output'].split("\n")[0] # Prevent continuous generation
         data['model_output'] = model_output
-        for x in id_metric[str(data['id'])]:
-            data['metric_en']= x[0]
-            data['metric_zh']= x[1]
-            tmp = copy.deepcopy(data)
-            results.append(tmp)
+        if str(data['id']) in id_metric:
+            for x in id_metric[str(data['id'])]:
+                data['metric_en']= x[0]
+                data['metric_zh']= x[1]
+                tmp = copy.deepcopy(data)
+                results.append(tmp)
 
 f= open(f"results/generation_trans.jsonl", 'w') 
 f.write(json.dumps(results, ensure_ascii=False, indent=4)) 
